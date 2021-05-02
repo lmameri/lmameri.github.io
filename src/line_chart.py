4284077645c42489fd6ec8e6cb5c476c2c35fde0
HeleneLy01 <77684443+HeleneLy01@users.noreply.github.com>
2021-05-01 21:46:19 -0400

    update hover

--- src/line_chart.py
+++ src/line_chart.py
@@ -5,7 +5,7 @@ import hovertemplate
 def get_linechart(df,media_list):
         fig = go.Figure()
         button_list = []
-        button_list.append(dict( args=[{'opacity': [1]*5}], label='Vue générale', method='restyle' ))
+        button_list.append(dict( args=[{'opacity': [1]*32}], label='Vue générale', method='restyle' ))
 
         for i in range(0,len(media_list)):
                 fig=fig.add_trace(go.Scatter(
@@ -13,7 +13,7 @@ def get_linechart(df,media_list):
                         y=df.loc[df['compte'] == media_list[i]]['MaxFollowers'],
                         mode="lines",
                         name=media_list[i],
-                        hovertemplate=linecharthovertemplate)) 
+                        hovertemplate=get_hovertemplate_linechart())) 
                 temp = [0.1] *(len(media_list)+1)
                 temp[i] = [1] 
                 button_list.append(dict( args=[{'opacity': temp}], label=media_list[i], method='restyle'))
